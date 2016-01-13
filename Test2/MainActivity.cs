46460bdd4bccf44b2392efce5d62dca669563b1e
Evgeny <e.chulak@hotmail.com>
2016-01-13 12:41:22 -0500

    test commit

--- Test2/MainActivity.cs
+++ Test2/MainActivity.cs
@@ -27,11 +27,11 @@ namespace Test2
 			Button buttonEvg = FindViewById<Button> (Resource.Id.button1);
 
 			button.Click += delegate {
-<<<<<<< HEAD
+
 				button.Text = string.Format ("{0} clicks! Hello from Evgeny and Rich!", count++);
-=======
-				button.Text = string.Format ("Hello from Evgeny!");
->>>>>>> ce42fa6034654f89bec0aa6cf56655a97f0eba2d
+
+				buttonEvg.Text = string.Format ("Hello from Evgeny!");
+
 			};
 		}
 	}
